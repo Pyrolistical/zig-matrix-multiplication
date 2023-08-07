5afd49f52697601ebf54ab79c62bccc4c1f20d4e
Ronald Chen <pyrogx1133@gmail.com>
2023-08-07 15:32:26 -0700

    expanded benchmark to scale n from powers of 2 from 2 to 2048

--- benchmark.zig
+++ benchmark.zig
@@ -6,25 +6,26 @@ pub fn main() !void {
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
-    inline for (common.variants) |variant| {
-        const allocMatmul = common.matmul_by_variant(variant);
-        const n = 48 * 40;
+    inline for (&[_]usize{ 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048 }) |n| {
+        inline for (common.variants) |variant| {
+            const allocMatmul = common.matmul_by_variant(variant);
 
-        var a = try allocator.alloc(f32, n * n);
-        defer allocator.free(a);
-        for (a) |*x| {
-            x.* = 1;
-        }
-        var b = try allocator.alloc(f32, n * n);
-        defer allocator.free(b);
-        for (b) |*x| {
-            x.* = 2;
-        }
+            var a = try allocator.alloc(f32, n * n);
+            defer allocator.free(a);
+            for (a) |*x| {
+                x.* = 1;
+            }
+            var b = try allocator.alloc(f32, n * n);
+            defer allocator.free(b);
+            for (b) |*x| {
+                x.* = 2;
+            }
 
-        const stdout = std.io.getStdOut().writer();
-        const start = std.time.milliTimestamp();
-        const c = try allocMatmul(n, allocator, a, b);
-        defer allocator.free(c);
-        nosuspend try stdout.print("{s} {} ms\n", .{ @tagName(variant), std.time.milliTimestamp() - start });
+            const stdout = std.io.getStdOut().writer();
+            const start = std.time.milliTimestamp();
+            const c = try allocMatmul(n, allocator, a, b);
+            defer allocator.free(c);
+            nosuspend try stdout.print("{},{s},{} ms\n", .{ n, @tagName(variant), std.time.milliTimestamp() - start });
+        }
     }
 }
